33f2bb223044232a51da93e0eafc6e42c4484bb2
ruan-cardozo <ruanvcardozo@gmail.com>
2023-08-08 20:50:21 -0300

    Editar exercico 2, tratar se numero for igual

--- 2.c
+++ 2.c
@@ -6,13 +6,15 @@ mostre no vídeo o valor das duas em seguida, a maior delas.
 #include <stdio.h>
 
 int main () {
-    int a = 10, b = 5;
+    int a = 6, b = 7;
 
     printf("A: %d\n", a);
     printf("B: %d\n", b);
 
     if (a > b) {
         printf("A é maior que B\n");
+    } else if (a == b) {
+        printf("A é igual a B\n");
     } else {
         printf("B é maior que A\n");
     }
